5cf3707b3e9e44c4bd90d7c64330c0433981b7a1
Mike <madetunji1376@gmail.com>
2022-07-28 15:38:50 +0100

    functionalities to consume the API and display the each score done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,50 @@
 // import _ from 'lodash';
-import './style.css';
+// import { method } from 'lodash';
+import './index.css';
+import './modules/Api-info.js';
+
+const refresh = document.querySelector('#refresh');
+const showPage = document.querySelector('#display-scores');
+
+const displayScore = async () => {
+  // fetching from the api using fetch() and awaiting the response
+  const response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/q6JDTAKTIqShhVc9wbL1/scores/'
+  );
+  // we want the response to be in .json() format
+  const totalResult = await response.json();
+  const arr = totalResult.result;
+  return arr;
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  let newArr = [];
+  try {
+    // set the newArr to await the displayScore
+    newArr = await displayScore();
+  } catch (error) {
+    // console.log('error 404');
+  }
+  newArr.forEach((element) => {
+    showPage.innerHTML += `<span id="each-score">${element.user}: ${element.score}</span>`;
+  });
+});
+
+refresh.addEventListener('click', async () => {
+  let newArr = [];
+  try {
+    //     // set the newArr to await the displayScore
+    newArr = await displayScore();
+  } catch (error) {
+    //     // console.log('error 404');
+  }
+
+  const domList = document.querySelectorAll('#each-score');
+
+  domList.forEach((e) => {
+    e.remove();
+  });
+  newArr.forEach((element) => {
+    showPage.innerHTML += `<span id="each-score">${element.user}: ${element.score}</span>`;
+  });
+});
---
+++ src/modules/Api-info.js
@@ -0,0 +1,25 @@
+const submitForm = document.querySelector('#user-form');
+const enterName = document.querySelector('#username');
+const enterScore = document.querySelector('#score');
+
+submitForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (enterName === '' || enterScore === '') return;
+
+  fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/q6JDTAKTIqShhVc9wbL1/scores/',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        user: enterName.value,
+        score: enterScore.value,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    }
+  );
+
+  enterName.value = '';
+  enterScore.value = '';
+});
